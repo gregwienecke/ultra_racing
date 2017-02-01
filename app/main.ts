9c617ac8d926691e72a52ee0fe3af94236ea646c
Greg Wienecke <gregwienecke@gmail.com>
2017-01-31 23:04:04 -0600

    added commas in carParts dictionaries

--- app/main.ts
+++ app/main.ts
@@ -27,21 +27,21 @@ class AppComponent {
 			"id": 1,
 			"name": "Super Tires",
 			"description": "These tires are the very best",
-			"inStock": 5
+			"inStock": 5,
 			"price": 4.99
 		},
 		{
 			"id": 2,
 			"name": "Reinforced Shocks",
 			"description": "Shocks made from kryptonite",
-			"inStock": 4
+			"inStock": 4,
 			"price": 9.99
 		},
 		{
 			"id": 3,
 			"name": "Padded Seats",
 			"description": "Super soft seats for a smooth ride",
-			"inStock": 0
+			"inStock": 0,
 			"price": 29.99
 		}
 	];
